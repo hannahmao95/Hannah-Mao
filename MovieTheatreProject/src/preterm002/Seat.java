c079b6a452145da6f8144ea7703b1b441f803ec4
Hannah Mao <hannahmao@Hannahs-MBP.lan>
2020-07-20 15:16:22 -0500

    Projects

---
+++ MovieTheatreProject/src/preterm002/Seat.java
@@ -0,0 +1,82 @@
+package preterm002;
+
+//A seat object associate the client's id with clinet's seating
+
+public class Seat {
+	
+	private String lastname;
+	private String firstname;
+    private int id;
+    int row;
+    int col;
+   
+
+	public Seat(String f, String l, int i, int r , int c) {
+		
+		firstname = f;
+		lastname=l;
+		id = i;	
+		row =r;
+		col= c;
+		
+	}
+	
+	public Seat() {
+		
+	}
+	
+	void setSeat( int newrow, int newcol) {
+		this.row = newrow;
+		this.col= newcol;
+	}
+	
+	
+	void setID(int newid) {
+		this.id= newid;
+	}
+		
+	String getID() {
+		
+		if(this.id<10) {
+	       String ID = "0"+Integer.toString(id);
+	     
+	       return ID;
+	       
+		}else {
+			
+			String ID = Integer.toString(id);
+			return ID;	
+		}			
+	}
+	
+	
+	String getSeat() {
+		
+		//return rows A-J, columns 1-10
+		String seat= (char)((row+1)+64) + "-" +(col+1);
+	
+		return seat;	
+	}
+	
+	String getFullname() {
+		
+		String full = (firstname + " "+ lastname);
+		return full;
+		
+	}	
+	
+      String getInitialIDString() {
+		
+		if(id<10) {
+		String full = ("id:" + '0'+id + "/" + firstname.charAt(0) + lastname.charAt(0));
+		return full;
+		}
+		else {
+			String full = ("id:" + id + "/" + firstname.charAt(0) + lastname.charAt(0));
+			
+			return full;
+			
+		}
+    }
+}
+
